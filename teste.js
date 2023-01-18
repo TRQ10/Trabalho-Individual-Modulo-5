0b2387e749e6c5c2f0db1f4c2559f199c9974e42
Tiago Roque <110204165+TRQ10@users.noreply.github.com>
2023-01-18 12:36:18 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,36 @@
+﻿const prompt = require('prompt-sync') ();
+
+const propriedades = []
+
+let condicional = false
+
+const comando = () => {
+    let propriedade = prompt("Digite uma propriedade de CSS: ");
+    propriedades.push(propriedade)  
+    return propriedade
+    
+}
+
+const pergunta = () => {
+    let opcao= prompt("Quer digitar uma propriedade [CONTINUAR/SAIR]? ").toUpperCase();
+    switch(opcao) {
+        case "SAIR":
+        condicional = true
+        propriedades.sort()
+        console.log(propriedades)
+        break
+    case "CONTINUAR":
+        condicional = false
+        comando()
+        break
+    default:
+        console.log("Opção inválida")
+    }
+    return opcao
+}
+
+comando()
+
+while (!condicional){
+    pergunta()
+}
